3c4bfecd8aab55f7a5aec1fbffecf9c3c11d1c15
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-01-11 21:31:45 +0530

    rough structure of guessing number done

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.guess').value = 99;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    displayMessage('📈 Too High!');
+  } else if (guess < secretNumber) {
+    displayMessage('📉 Too Low!');
+  }
+});
