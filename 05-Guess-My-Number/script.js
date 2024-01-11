82eb1b954106d666cfdf403f32ea356ab7a231c3
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-01-12 02:34:08 +0530

    highscore logic done

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,6 +8,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
@@ -25,6 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       displayMessage('📈 Too High!');
