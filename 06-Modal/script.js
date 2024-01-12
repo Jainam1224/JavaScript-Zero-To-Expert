4b42e155a88095d81fcf6795b2d48b6fc098058d
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-01-12 15:04:32 +0530

    opening closing modal clicking on overlay and cross button

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,3 +4,19 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
