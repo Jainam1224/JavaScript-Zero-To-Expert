ffca69e49dd1f59a600e47100cdfb72c910635bd
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-02-04 23:35:34 +0530

    adding new display movement i.e. new statement of transaction

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -62,6 +62,30 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
+// Functions
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html); // Used to insert the new HTML element
+  });
+};
+
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
