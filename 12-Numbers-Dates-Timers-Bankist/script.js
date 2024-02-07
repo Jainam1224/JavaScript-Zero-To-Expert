04b0dc2cc19e5c73219f69d55139d1abf957e611
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-02-07 10:38:44 +0530

    mumeric separtor

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -368,3 +368,24 @@ labelBalance.addEventListener("click", function () {
   });
 });
 */
+
+/*
+///////////////////////////////////////
+// Numeric Separators
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter); // 287460000000 -> ignores _
+
+const price = 345_99;
+console.log(price); // 34599
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI); // 3.1415
+
+console.log(Number("230_000")); // NaN
+console.log(parseInt("230_000")); // 230
+*/
