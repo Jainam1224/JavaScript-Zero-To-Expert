06f5185b336ecee187c253502fdb96fa16207909
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2024-02-07 12:33:37 +0530

    bigint

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -389,3 +389,38 @@ console.log(PI); // 3.1415
 console.log(Number("230_000")); // NaN
 console.log(parseInt("230_000")); // 230
 */
+
+/*
+///////////////////////////////////////
+// Working with BigInt
+console.log(2 ** 53 - 1); // 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+console.log(2 ** 53 + 1); // might be incorrect
+console.log(2 ** 53 + 2); // might be incorrect
+console.log(2 ** 53 + 3); // might be incorrect
+console.log(2 ** 53 + 4); // might be incorrect
+
+console.log(4838430248342043823408394839483204n); // 4838430248342043823408394839483204n
+console.log(BigInt(48384302)); // 48384302n => Using BigInt also we can't define any big number
+
+// Operations
+console.log(10000n + 10000n); // 20000n
+console.log(36286372637263726376237263726372632n * 10000000n); // 362863726372637263762372637263726320000000n
+// console.log(Math.sqrt(16n));
+
+const huge = 20289830237283728378237n;
+const num = 23;
+console.log(huge * BigInt(num)); // 466666095457525752699451n
+
+// Exceptions
+console.log(20n > 15); // true
+console.log(20n === 20); // false
+console.log(typeof 20n); // bigint
+console.log(20n == "20"); // true
+
+console.log(huge + " is REALLY big!!!"); // 20289830237283728378237 is REALLY big!!!
+
+// Divisions
+console.log(11n / 3n); // 3n
+console.log(10 / 3); // 3.3333...
+*/
